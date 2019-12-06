6fa1990cf3b6e32157b44945af0c31b7e232ce2d
kozlovcode <romandkoz@gmail.com>
2019-12-06 03:35:26 +0300

    Initial app

---
+++ app.py
@@ -0,0 +1,24 @@
+import os
+import telebot
+import requests
+
+TOKEN = os.environ['TOKEN']
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+	bot.send_message(message.chat.id, 'Отправь мне голосовое сообщение, а я верну аудиофайл, чтобы ты смог его сохранить')
+
+@bot.message_handler(content_types=['voice'])
+def echo_all(message):
+	voice_id = message.voice.file_id
+	file_info = bot.get_file(message.voice.file_id)
+	file = requests.get('https://api.telegram.org/file/bot{0}/{1}'.format(TOKEN, file_info.file_path))
+	open(f'{voice_id}.oga', 'wb').write(file.content)
+	os.rename(rf'{voice_id}.oga',rf'{voice_id}.mp3')
+	doc = open(f'{voice_id}.mp3', 'rb')
+	bot.send_audio(message.chat.id, doc)
+	doc.close()
+	os.remove(f'{voice_id}.mp3')
+
+bot.polling()
